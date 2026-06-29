21b7017a1018721147d21236cd6ce07365147f3d
Pavel <pashakuz1022@gmail.com>
2026-06-29 21:59:22 +0300

    таблица БД

---
+++ DB_crypto.sql
@@ -0,0 +1,9 @@
+CREATE TABLE prices(
+    id SERIAL PRIMARY KEY,
+    symbol VARCHAR(20) NOT NULL,
+    price NUMERIC(20, 8) NOT NULL,
+    volume NUMERIC(20, 8),
+    fetched_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+SELECT * FROM prices
