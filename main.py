f39a85fa598387fe8cfc6377267c293d4281be89
Pavel <pashakuz1022@gmail.com>
2026-07-30 21:28:12 +0300

    Добавлены комментарии

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-from BD_connect import conn
+import BD_connect
 import ccxt
 import time
 
-##
+## Получение массива данных крипты
 exchenge = ccxt.bybit()
 
-##
-curr = conn.cursor()
+## Подключение к БД
+curr = BD_connect.conn.cursor()
 
-##
+## Функция, которая заносит из массива в таблицу каждые 60 секунд
 while True:
     try:
         ticker = exchenge.fetch_ticker('BTC/USDT')
         curr.execute(
             "INSERT INTO prices (symbol, price, volume) VALUES (%s, %s, %s)",
             (ticker['symbol'], ticker['last'], ticker['baseVolume']))
-        conn.commit()
+        BD_connect.conn.commit()
         print(f"Записано: {ticker['symbol']} = {ticker['last']}")
     except Exception as e:
         print(f"Ошибка: {e}")
